2025312b07f3158210effdc324d32dde8a956db2
Nuradil <mr.nuradil@gmail.com>
2020-10-18 16:21:25 +0600

    add signup and signin post methods vol.2

--- src/main/java/net/javaguides/springboot/controller/UserController.java
+++ src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -1,14 +1,21 @@
 package net.javaguides.springboot.controller;
 
 import net.javaguides.springboot.model.User;
+import net.javaguides.springboot.payload.request.LoginRequest;
+import net.javaguides.springboot.payload.request.SignupRequest;
+import net.javaguides.springboot.payload.response.JwtResponse;
+import net.javaguides.springboot.payload.response.MessageResponse;
 import net.javaguides.springboot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/auth/")
 public class UserController {
 
     @Autowired
@@ -20,9 +27,39 @@ public class UserController {
         return userRepository.findAll();
     }
 
-//  create user rest api
-    @PostMapping("/users")
-    public User createUser(@RequestBody User user){
-        return userRepository.save(user);
+    @PostMapping("/signup")
+    public ResponseEntity<?> singup(@Valid @RequestBody SignupRequest signUpRequest){
+        List<User> users = this.getAllUsers();
+
+        for(User user: users){
+            if(user.getEmail().equals(signUpRequest.getEmail())){
+                return ResponseEntity.badRequest().body(new MessageResponse("A user with this email is registered!"));
+            }
+            if(user.getUsername().equals(signUpRequest.getUsername())){
+                return ResponseEntity.badRequest().body(new MessageResponse("A user with this username is registered"));
+            }
+        }
+
+        userRepository.save(new User(signUpRequest.getUsername(), signUpRequest.getPassword(), signUpRequest.getEmail()));
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> signin(@Valid @RequestBody LoginRequest loginRequest){
+        if(!userRepository.existsByUsername(loginRequest.getUsername())){
+            return ResponseEntity.badRequest().body(new MessageResponse("Incorrect login or password"));
+        }
+
+        Optional<User> user = userRepository.findByUsername(loginRequest.getUsername());
+
+        if(!user.get().getPassword().equals(loginRequest.getPassword())){
+            return ResponseEntity.badRequest().body(new MessageResponse("Incorrect login or password"));
+        }
+
+        return ResponseEntity.ok(new JwtResponse(
+                user.get().getId(),
+                user.get().getUsername(),
+                user.get().getEmail()));
     }
 }
--- src/main/java/net/javaguides/springboot/payload/response/JwtResponse.java
+++ src/main/java/net/javaguides/springboot/payload/response/JwtResponse.java
@@ -1,2 +1,39 @@
-package net.javaguides.springboot.payload.response;public class JwtResponse {
+package net.javaguides.springboot.payload.response;
+
+import java.util.List;
+
+public class JwtResponse {
+    private Long id;
+    private String username;
+    private String email;
+
+    public JwtResponse(Long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
