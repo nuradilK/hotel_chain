ed3c2d0ae87681ca1b692e3b74cfeb835e3516cb
Nuradil <mr.nuradil@gmail.com>
2020-10-18 16:18:01 +0600

    add signin and singup post methods

---
+++ src/main/java/net/javaguides/springboot/payload/request/LoginRequest.java
@@ -0,0 +1,2 @@
+package net.javaguides.springboot.payload.request;public class LoginRequest {
+}
---
+++ src/main/java/net/javaguides/springboot/payload/response/MessageResponse.java
@@ -0,0 +1,2 @@
+package net.javaguides.springboot.payload.response;public class MessageResponse {
+}
